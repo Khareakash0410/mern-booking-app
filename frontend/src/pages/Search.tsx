5ebc55befd3b166204a19072a209ce3c4d485e98
Akash Khare <akashkhare706882@gmail.com>
2024-07-31 11:49:58 +0530

    responsive remove sticky

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -57,7 +57,7 @@ const handleFacilityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
 
-       <div className="border border-slate-300 p-5 h-fit sticky top-10 rounded-lg">
+       <div className="border border-slate-300 p-5 h-fit lg:sticky top-10 rounded-lg">
             <div className="space-y-5">
                <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">Filter By:</h3>
                <StarRatingFilter
